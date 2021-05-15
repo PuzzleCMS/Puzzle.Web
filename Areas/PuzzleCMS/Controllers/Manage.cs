b93df8c0b6135c0e25e5637b212fd79fbe5b3b5c
SirPatagon <sirpatagon+development@gmail.com>
2021-05-15 14:51:37 +0200

    Copied project into own repository

---
+++ Areas/PuzzleCMS/Controllers/Manage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Puzzle.Web.Areas.PuzzleCMS.Controllers
+{
+    public class Manage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
